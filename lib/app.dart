3aba541bb8b498ada111d8717534df8a3815315d
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2020-06-29 10:05:54 +0200

    Fix name

--- lib/app.dart
+++ lib/app.dart
@@ -6,7 +6,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ParaGuez',
+      title: 'GeoCar',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
